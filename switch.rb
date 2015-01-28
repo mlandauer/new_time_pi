ad4723e355cfb45aa3ea33651462d59f4e5973b3
Matthew Landauer <matthew@oaf.org.au>
2015-01-28 16:25:52 +1100

    Don't access hardware in development

--- switch.rb
+++ switch.rb
@@ -3,21 +3,34 @@ require 'pi_piper'
 
 # Control a physical clock by moving the second hand forward
 class Clock
-  def initialize
-    @pin1 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-    @pin2 = PiPiper::Pin.new(:pin => 27, :direction => :out)
-    @pin1.off
-    @pin2.off
+  attr_accessor :development
+
+  def initialize(development = false)
+    @development = development
+    if !development
+      @pin1 = PiPiper::Pin.new(:pin => 17, :direction => :out)
+      @pin2 = PiPiper::Pin.new(:pin => 27, :direction => :out)
+      @pin1.off
+      @pin2.off
+    end
     @count = 0
   end
 
   # Move the second hand forward
   def tick
     if @count == 0
-      pulse_pin(@pin1)
+      if development
+        puts "Tick..."
+      else
+        pulse_pin(@pin1)
+      end
       @count = 1
     else
-      pulse_pin(@pin2)
+      if development
+        puts "Tock..."
+      else
+        pulse_pin(@pin2)
+      end
       @count = 0
     end
   end

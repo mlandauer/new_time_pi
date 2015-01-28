2dde863e8925b192f0e4053befb97079644f4497
Matthew Landauer <matthew@oaf.org.au>
2015-01-28 17:03:53 +1100

    Set rate of clock

--- switch.rb
+++ switch.rb
@@ -2,7 +2,14 @@
 require_relative "lib/clock"
 
 clock = Clock.new
+
+time_between_ticks = 0.5
+
+start = Time.now
 loop do
   clock.tick
-  sleep 0.02
+  t = Time.now - start
+  ticks = t / time_between_ticks
+  wait = ticks.ceil * time_between_ticks - t
+  sleep wait
 end

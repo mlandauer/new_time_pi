7c4d756278fa0b4c861398275a67e05cf0ac296e
Matthew Landauer <matthew@oaf.org.au>
2015-01-28 05:15:16 +0000

    Extract clock movement into class

--- switch.rb
+++ switch.rb
@@ -1,22 +1,33 @@
 #!/usr/bin/env ruby
 require 'pi_piper'
 
-pin1 = PiPiper::Pin.new(:pin => 17, :direction => :out)
-pin2 = PiPiper::Pin.new(:pin => 27, :direction => :out)
-pin1.off
-pin2.off
-
-pin = pin1
-
-loop do
-  # Turn pin1 on for a short time
-  pin.on
-  sleep 0.05
-  pin.off
-  if pin == pin1
-    pin = pin2
-  else
-    pin = pin1
+# Control a physical clock by moving the second hand forward
+class Clock
+  def initialize
+    @pin1 = PiPiper::Pin.new(:pin => 17, :direction => :out)
+    @pin2 = PiPiper::Pin.new(:pin => 27, :direction => :out)
+    @pin1.off
+    @pin2.off
+    @pin = @pin1
   end
+
+  # Move the second hand forward
+  def tick
+    # Turn pin1 on for a short time
+    @pin.on
+    sleep 0.05
+    @pin.off
+    if @pin == @pin1
+      @pin = @pin2
+    else
+      @pin = @pin1
+    end
+  end
+end
+
+
+clock = Clock.new
+loop do
+  clock.tick
   sleep 0.02 
 end
